a3d732b87a5c5c6fd8ee4d5fe5e06fe08a625902
m0_73374301 <m0_73374301@noreply.gitcode.com>
2025-10-31 20:30:48 +0800

    learning softmax regression scratch

--- 3.linear-network/3.5image-classification-dataset.py
+++ 3.linear-network/3.5image-classification-dataset.py
@@ -73,10 +73,11 @@ def load_data_fashion_mnist(batch_size, resize=None):  #@save
     if resize:
         trans.insert(0, transforms.Resize(resize))
     trans = transforms.Compose(trans)
+    # 这里是同位置的 data，d2l 库中是与父级同级
     mnist_train = torchvision.datasets.FashionMNIST(
-        root="../data", train=True, transform=trans, download=True)
+        root="data", train=True, transform=trans, download=True)
     mnist_test = torchvision.datasets.FashionMNIST(
-        root="../data", train=False, transform=trans, download=True)
+        root="data", train=False, transform=trans, download=True)
     return (data.DataLoader(mnist_train, batch_size, shuffle=True,
                             num_workers=get_dataloader_workers()),
             data.DataLoader(mnist_test, batch_size, shuffle=False,
