484b3ffc82344e223880828faf94e57e69156fcf
m0_73374301 <1144841911@qq.com>
2025-11-13 20:09:54 +0800

    custom layer

--- 5.dl-computation/5.4custom-layer.py
+++ 5.dl-computation/5.4custom-layer.py
@@ -1,3 +1,32 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
+
+
+class CenteredLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    # 这一层用于对每一项减去平均数
+    def forward(self, X):
+        return X - X.mean()
+
+
+layer = CenteredLayer()
+# print(layer(torch.FloatTensor([1, 2, 3, 4, 5])))
+
+
+# 带参的层
+class MyLinear(nn.Module):
+    def __init__(self, in_units, units):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(in_units, units))
+        self.bias = nn.Parameter(torch.randn(units, ))
+
+    def forward(self, X):
+        linear = torch.matmul(X, self.weight.data) + self.bias.data
+        return F.relu(linear)
+
+
+linear = MyLinear(5, 3)
+print(linear.weight)
