247669d2cc9bacfd9cdbbfd393b5ba300c5ac447
m0_73374301 <1144841911@qq.com>
2025-11-13 20:36:02 +0800

    read and write

---
+++ 5.dl-computation/5.5read-write.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+# x = torch.arange(4)
+# torch.save(x, 'x-file')
+# x2 = torch.load('x-file')
+# print(x2)
+# y = torch.zeros(4)
+# torch.save([x, y], 'x-file')
+# x2, y2 = torch.load('x-file')
+# print(x2, y2)
+# torch.save({'x': x, 'y': y}, 'x-file')
+# print(torch.load('x-file'))
+
+
+# 存取模型参数
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden = nn.Linear(20, 256)
+        self.output = nn.Linear(256, 10)
+
+    def forward(self, x):
+        return self.output(F.relu(self.hidden(x)))
+
+
+net = MLP()
+X = torch.randn(size=(2, 20))
+Y = net(X)
+torch.save(net.state_dict(), 'mlp.params')
+
+clone = MLP()
+clone.load_state_dict(torch.load('mlp.params'))
+# print(clone.eval())
+
+Y_clone = clone(X)
+print(Y_clone == Y)
